c338b2a1209dcf2e489216b44f7c539be890f327
Ralh19 <ruben.almeidaalhada@gmail.com>
2024-12-13 12:37:49 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,57 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      screens: {
+        sm: "480px",
+      },
+      colors: {
+        "dark-purple-200": "#353353",
+        "dark-purple-300": "#1e1d30",
+        "pink-red": "rgb(176, 57, 83)",
+        "pink-red-light": "rgb(206, 100, 123)",
+      },
+      fontFamily: {
+        nunito: ["Nunito", "sans-serif"],
+      },
+      dropShadow: {
+        glow: [
+          "0 0px 20px rgba(255,255, 255, 0.35)",
+          "0 0px 65px rgba(255, 255,255, 0.2)",
+        ],
+      },
+      animation: {
+        "zoom-in": "zoomIn 0.3s ease-in-out forwards", // Animation de zoom-in
+        "zoom-out": "zoomOut 0.3s ease-in-out forwards", // Animation de zoom-out
+      },
+      keyframes: {
+        zoomIn: {
+          "0%": {
+            transform: "scale(0.8)",
+            opacity: "0",
+          },
+          "100%": {
+            transform: "scale(1)",
+            opacity: "1",
+          },
+        },
+        zoomOut: {
+          "0%": {
+            transform: "scale(1)",
+            opacity: "1",
+          },
+          "100%": {
+            transform: "scale(0.8)",
+            opacity: "0",
+          },
+        },
+      },
+      textShadow: {
+        pink_red_glow: "0 0 5px #b03953, 0 0 10px #b03953, 0 0 20px #b03953, 0 0 40px #b03953, 0 0 80px #b03953",
+        white_glow: "0 0 5px #b5b5b5, 0 0 10px #b5b5b5, 0 0 20px #b5b5b5, 0 0 40px #b5b5b5, 0 0 80px #b5b5b5",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-textshadow")],
+};
