6de72c0db3ac29f9d3b44fde6855b15c0aa94337
Ralh19 <ruben.almeidaalhada@gmail.com>
2024-12-13 15:29:45 +0100

    re-design of the modal system

--- dist/scripts/modal.js
+++ dist/scripts/modal.js
@@ -1,18 +1,20 @@
 // Fonction pour ouvrir le modal
+
+const modalStructure = document.getElementById("modal-structure");
+const modal = document.getElementById("modal");
+
 function openModal(imageSrc, description) {
-  const modal = document.getElementById('modal');
-  const modalImg = document.getElementById('modal-img');
-  const modalDescription = document.getElementById('modal-description');
+  const modalImg = document.getElementById("modal-img");
+  const modalDescription = document.getElementById("modal-description");
 
   modalImg.src = imageSrc; // Mettre à jour l'image du modal
   modalDescription.textContent = description; // Mettre à jour la description du modal
-  modal.classList.remove('hidden'); // Retire "hidden" pour afficher
-  modal.classList.add('flex'); // Ajoute "flex" pour centrer
+  modal.classList.remove("hidden"); // Retire "hidden" pour afficher
+  modal.classList.add("flex"); // Ajoute "flex" pour centrer
 }
-
-// Fonction pour fermer le modal
-document.getElementById('close-modal').addEventListener('click', function() {
-  const modal = document.getElementById('modal');
-  modal.classList.add('hidden'); // Ajoute "hidden" pour masquer
-  modal.classList.remove('flex'); // Retire "flex" lorsqu'on cache le modal
+modalStructure.addEventListener("click", (event) => {
+  if (event.target === modalStructure) {
+    modal.classList.add("hidden"); // Ajoute "hidden" pour masquer
+    modal.classList.remove("flex"); // Retire "flex" lorsqu'on cache le modal
+  }
 });
